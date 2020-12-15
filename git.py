89ea95c5e133ab29aad974af3928da5c0cd08b00
dataxsaiyan3 <data.saiyan3@gmail.com>
2020-12-15 15:03:31 +0200

    New code

---
+++ git.py
@@ -0,0 +1,7 @@
+# Code command for github that solved please tell me who you are issue
+1.git init
+2.git config user.name "someone"
+3.git config user.email "someone@someplace.com"
+4.git add *
+5.git commit -m "some init msg"
+
