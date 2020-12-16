7c09fe356f2103d5666ca70362dd3334fd7237c8
dataxsaiyan3 <data.saiyan3@gmail.com>
2020-12-16 16:23:37 +0200

    Basic command for in Git Bash

--- git.py
+++ git.py
@@ -1,3 +1,12 @@
+# Basic commands to git and push 
+git init
+git add README.md
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/userName/repoName.git
+git push --force origin master
+
+
 # Code command for github that solved please tell me who you are issue
 1.git init
 2.git config user.name "someone"
@@ -5,5 +14,17 @@
 4.git add *
 5.git commit -m "some init msg"
 
-#This code will let git log display better when used
+# This code will let git log display better when used
 git log --topo-order --all --graph --date=local --pretty=format:'%C(green)%h%C(reset) %><(55,trunc)%s%C(red)%d%C(reset) %C(blue)[%an]%C(reset) %C(yellow)%ad%C(reset)%n'
+
+
+# This code let you fix rejection issues
+git fetch origin master:tmp
+git rebase tmp
+git push origin HEAD:master
+git branch -D tmp
+
+# This code let you pick GitHub branch name
+$ git branch -m master
+
+
